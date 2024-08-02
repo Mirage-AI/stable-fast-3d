860c11767d1722728d7ccdbd0f3f721b3aa68262
Carlos Diez <cdiezmoran@gmail.com>
2024-08-01 22:09:51 -0600

    Remove github url from requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-# Function to read the requirements.txt file
 def parse_requirements(filename):
     with open(filename, 'r') as file:
         return file.read().splitlines()
@@ -9,8 +8,10 @@ setup(
     name='sf3d',
     version='0.1.0',
     description='Stable Fast 3D',
-    url='https://github.com/Mirage-AI/stable-fast-3d',  # Update with your project's URL
+    url='https://github.com/Mirage-AI/stable-fast-3d',
     packages=find_packages(),
-    install_requires=parse_requirements('requirements.txt'),
+    install_requires=parse_requirements('requirements.txt') + [
+        'git+https://github.com/vork/PyNanoInstantMeshes.git'
+    ],
     python_requires='>=3.6',
 )
